367a7e2d3954b384da923d216a695171c2ef614e
Junsung Choi <choiexe1@gmail.com>
2025-04-11 22:15:21 +0900

    feat: add common color, common text style

---
+++ lib/ui/ui_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class UIColor {
+  static const Color primary = Color(0xFF0D1B2A);
+  static const Color secondary = Color(0xFFD4AF37);
+  static const Color tertiary = Color(0xFF3E6C9E);
+  static const Color quaternary = Color(0xFFA39EC4);
+  static const Color quinary = Color(0xFF3B2C24);
+}
