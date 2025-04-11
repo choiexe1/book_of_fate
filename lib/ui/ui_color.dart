0c8caee3b5bdb1b7cb66ed8de1578acb9337848a
Junsung Choi <choiexe1@gmail.com>
2025-04-12 00:53:00 +0900

    refactor: change variable names

--- lib/ui/ui_color.dart
+++ lib/ui/ui_color.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 abstract class UIColor {
-  static const Color primary = Color(0xFF0D1B2A);
-  static const Color secondary = Color(0xFFD4AF37);
-  static const Color tertiary = Color(0xFF3E6C9E);
-  static const Color quaternary = Color(0xFFA39EC4);
-  static const Color quinary = Color(0xFF3B2C24);
+  static const Color darkBlue = Color(0xFF0D1B2A);
+  static const Color gold = Color(0xFFD4AF37);
+  static const Color blue = Color(0xFF3E6C9E);
+  static const Color lavender = Color(0xFFA39EC4);
+  static const Color brown = Color(0xFF3B2C24);
+  static const Color white = Color(0xFFFFFFFF);
 }
