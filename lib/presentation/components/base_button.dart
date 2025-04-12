eb59a39a3714cf05ad0f12457c1517ec6900e4c5
Junsung Choi <choiexe1@gmail.com>
2025-04-12 22:30:29 +0900

    feat: base button component

---
+++ lib/presentation/components/base_button.dart
@@ -0,0 +1,53 @@
+import 'package:book_of_fate/ui/ui_color.dart';
+import 'package:book_of_fate/ui/ui_text.dart';
+import 'package:flutter/material.dart';
+
+class BaseButton extends StatefulWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const BaseButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  State<BaseButton> createState() => _BaseButtonState();
+}
+
+class _BaseButtonState extends State<BaseButton>
+    with SingleTickerProviderStateMixin {
+  double _scale = 1.0;
+
+  void _onTapDown(TapDownDetails details) {
+    setState(() => _scale = 0.97);
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    setState(() => _scale = 1.0);
+    widget.onPressed();
+  }
+
+  void _onTapCancel() {
+    setState(() => _scale = 1.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTapCancel: _onTapCancel,
+      child: AnimatedScale(
+        scale: _scale,
+        duration: Duration(milliseconds: 100),
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 14),
+          decoration: BoxDecoration(
+            color: UIColor.softGold,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Center(child: Text(widget.text, style: UIText.mediumTextBold)),
+        ),
+      ),
+    );
+  }
+}
