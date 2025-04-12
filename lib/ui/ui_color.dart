f57c663e8e163f7469adb20f4d2f4bd798b116c2
Junsung Choi <choiexe1@gmail.com>
2025-04-12 22:27:29 +0900

    feat: add ui colors

--- lib/ui/ui_color.dart
+++ lib/ui/ui_color.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 
 abstract class UIColor {
   static const Color darkBlue = Color(0xFF0D1B2A);
+  static const Color deepNavy = Color(0xFF1A1C2C);
   static const Color gold = Color(0xFFD4AF37);
+  static const Color softGold = Color(0xFFE5C07B);
   static const Color blue = Color(0xFF3E6C9E);
   static const Color lavender = Color(0xFFA39EC4);
   static const Color brown = Color(0xFF3B2C24);
   static const Color white = Color(0xFFFFFFFF);
+  static const Color gray = Color(0xFFD5D3CA);
+  static const Color black = Color(0xFF000000);
 }
