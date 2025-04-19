417913aaadacdd7f8132a3a53e058a8a6abec416
Junsung Choi <choiexe1@gmail.com>
2025-04-19 21:56:47 +0900

    feat: splash screen

--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -1,8 +1,6 @@
 import 'package:book_of_fate/core/routes.dart';
-import 'package:book_of_fate/presentation/components/start_button.dart';
-import 'package:book_of_fate/presentation/components/custom_scaffold.dart';
-import 'package:book_of_fate/ui/ui_color.dart';
-import 'package:book_of_fate/ui/ui_text.dart';
+import 'package:book_of_fate/ui/app_color.dart';
+import 'package:book_of_fate/ui/app_font.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -13,126 +11,49 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> _slideAnimation;
-  late Animation<double> _fadeAnimation;
-
-  bool _isInitialized = false;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1200),
-    );
-
-    _slideAnimation = Tween<double>(begin: 200.0, end: 0.0).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.easeOutCubic),
-    );
-
-    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.easeIn),
-    );
-
-    _startInitializationProcess();
-  }
-
-  Future<void> _startInitializationProcess() async {
-    try {
-      // await Future.delayed(const Duration(seconds: 2));
-
-      if (mounted) {
-        setState(() {
-          _isInitialized = true;
-        });
-
-        _animationController.forward();
-      }
-    } catch (e) {
-      if (mounted) {
-        setState(() {
-          _isInitialized = true;
-        });
-        _animationController.forward();
-      }
-    }
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
+class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScaffold(
-      backgroundImage: 'assets/images/splash.png',
-      colorFilter: exposure(1.9),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          _isInitialized
-              ? AnimatedBuilder(
-                animation: _animationController,
-                builder: (context, child) {
-                  return Transform.translate(
-                    offset: Offset(0, _slideAnimation.value),
-                    child: Opacity(
-                      opacity: _fadeAnimation.value,
-                      child: Column(
-                        children: [
-                          Text(
-                            'Book of Fate',
-                            style: UIText.titleTextBold.copyWith(
-                              color: UIColor.softGold,
-                              shadows: [
-                                Shadow(
-                                  color: UIColor.black.withAlpha(100),
-                                  offset: Offset(0, 4),
-                                  blurRadius: 8,
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.50,
-                          ),
-                          Center(
-                            child: StartButton(
-                              text: '시작하기',
-                              onPressed: () => context.go(Routes.register),
-                              width: MediaQuery.of(context).size.width * 0.8,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              )
-              : const Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                    ),
-                    SizedBox(height: 16),
-                  ],
-                ),
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(Duration(seconds: 1), () {
+        // ignore: use_build_context_synchronously
+        context.go(Routes.home);
+      });
+    });
+
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: const AssetImage('assets/images/splash.png'),
+            colorFilter: buildColorFilter(1.7),
+            fit: BoxFit.cover,
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            const SizedBox(height: 150),
+            Text(
+              'Book of Fate',
+              style: AppFont.bold.copyWith(fontSize: 48, color: AppColor.white),
+            ),
+            Text(
+              'Discover your destiny',
+              style: AppFont.bold.copyWith(
+                fontSize: 18,
+                color: AppColor.softGold,
               ),
-          const SizedBox(height: 180),
-        ],
+            ),
+            const SizedBox(height: 400),
+            const CircularProgressIndicator(color: AppColor.softGold),
+          ],
+        ),
       ),
     );
   }
 
-  ColorFilter exposure(double exposure) {
+  ColorFilter buildColorFilter(double exposure) {
     return ColorFilter.matrix([
       exposure,
       0,
