69dde5d7378e363a9152fe59a41eb8e2c4c607bf
Junsung Choi <choiexe1@gmail.com>
2025-04-19 22:23:42 +0900

    feat: 필요 없는 스크린 제거

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -1,6 +1,5 @@
 import 'package:book_of_fate/core/routes.dart';
 import 'package:book_of_fate/presentation/home/home_screen.dart';
-import 'package:book_of_fate/presentation/register/register_screen.dart';
 import 'package:book_of_fate/presentation/splash/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -10,10 +9,6 @@ final appRouter = GoRouter(
     GoRoute(
       path: Routes.splash,
       builder: (context, state) => const SplashScreen(),
-    ),
-    GoRoute(
-      path: Routes.register,
-      builder: (context, state) => const RegisterScreen(),
     ),
     GoRoute(path: Routes.home, builder: (context, state) => const HomeScreen()),
   ],
