1c79aa346c2aa42f0ff2dba2da19738953206da7
Junsung Choi <choiexe1@gmail.com>
2025-04-19 21:31:52 +0900

    feat: 앱 폰트를 pretendard로 변경

--- lib/ui/app_font.dart
+++ lib/ui/app_font.dart
@@ -1,83 +1,20 @@
 import 'package:flutter/material.dart';
 
 abstract class AppFont {
-  static const String font = 'Poppins';
+  static const String pretendard = 'Pretendart';
 
-  static const TextStyle titleTextBold = TextStyle(
-    fontSize: 50,
+  static const TextStyle regular = TextStyle(
+    fontFamily: pretendard,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle semiBold = TextStyle(
+    fontFamily: pretendard,
     fontWeight: FontWeight.w600,
-    fontFamily: font,
   );
 
-  static const TextStyle headerTextBold = TextStyle(
-    fontSize: 30,
-    fontWeight: FontWeight.w600,
-    fontFamily: font,
-  );
-
-  static const TextStyle largeTextBold = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.w600,
-    fontFamily: font,
-  );
-
-  static const TextStyle mediumTextBold = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.w600,
-    fontFamily: font,
-  );
-
-  static const TextStyle normalTextBold = TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.w600,
-    fontFamily: font,
-  );
-
-  static const TextStyle smallTextBold = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.w600,
-    fontFamily: font,
-  );
-
-  static const TextStyle titleTextRegular = TextStyle(
-    fontSize: 50,
-    fontWeight: FontWeight.w400,
-    fontFamily: font,
-  );
-
-  static const TextStyle headerTextRegular = TextStyle(
-    fontSize: 30,
-    fontWeight: FontWeight.w400,
-    fontFamily: font,
-  );
-
-  static const TextStyle largeTextRegular = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.w400,
-    fontFamily: font,
-  );
-
-  static const TextStyle mediumTextRegular = TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeight.w400,
-    fontFamily: font,
-  );
-
-  static const TextStyle normalTextRegular = TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.w400,
-    fontFamily: font,
-  );
-
-  static const TextStyle smallTextRegular = TextStyle(
-    fontSize: 14,
-    fontWeight: FontWeight.w400,
-    fontFamily: font,
-  );
-
-  static const TextStyle labelTextBold = TextStyle(
-    fontSize: 10,
-    fontWeight: FontWeight.w400,
-    fontFamily: font,
+  static const TextStyle bold = TextStyle(
+    fontFamily: pretendard,
+    fontWeight: FontWeight.w700,
   );
 }
