345c2509da3f8dcc305faa6216cd69b22ac4c260
Junsung Choi <choiexe1@gmail.com>
2025-04-19 21:57:36 +0900

    feat: 폰트 적용

--- lib/ui/app_font.dart
+++ lib/ui/app_font.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
 abstract class AppFont {
-  static const String pretendard = 'Pretendart';
+  static const String _pretendard = 'Pretendart';
 
   static const TextStyle regular = TextStyle(
-    fontFamily: pretendard,
+    fontFamily: _pretendard,
     fontWeight: FontWeight.w400,
   );
 
   static const TextStyle semiBold = TextStyle(
-    fontFamily: pretendard,
+    fontFamily: _pretendard,
     fontWeight: FontWeight.w600,
   );
 
   static const TextStyle bold = TextStyle(
-    fontFamily: pretendard,
+    fontFamily: _pretendard,
     fontWeight: FontWeight.w700,
   );
 }
