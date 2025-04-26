805ce21bdff722075aa4bbe905e0e6c7a82491ce
Junsung Choi <choiexe1@gmail.com>
2025-04-26 21:31:48 +0900

    feat: 관리하기 쉽도록 di 변경

--- lib/core/di.dart
+++ lib/core/di.dart
@@ -1,6 +1,7 @@
 import 'package:book_of_fate/data/data_source/answer_data_source.dart';
 import 'package:book_of_fate/data/data_source/local/local_answer_data_source.dart';
 import 'package:book_of_fate/data/repository/answer_repository_impl.dart';
+import 'package:book_of_fate/data/use_case/get_random_answer_use_case.dart';
 import 'package:book_of_fate/domain/repository/answer_repository.dart';
 import 'package:book_of_fate/presentation/answer/answer_view_model.dart';
 import 'package:get_it/get_it.dart';
@@ -8,7 +9,28 @@ import 'package:get_it/get_it.dart';
 GetIt getIt = GetIt.instance;
 
 void diSetUp() {
+  _dataSources();
+  _repositories();
+  _useCases();
+  _viewModels();
+}
+
+void _dataSources() {
   getIt.registerSingleton<AnswerDataSource>(LocalAnswerDataSource());
+}
+
+void _repositories() {
   getIt.registerSingleton<AnswerRepository>(AnswerRepositoryImpl(getIt()));
-  getIt.registerFactory(() => AnswerScreenViewModel());
+}
+
+void _useCases() {
+  getIt.registerSingleton<GetRandomAnswerUseCase>(
+    GetRandomAnswerUseCase(getIt()),
+  );
+}
+
+void _viewModels() {
+  getIt.registerFactory(
+    () => AnswerScreenViewModel(getRandomAnswerUseCase: getIt()),
+  );
 }
